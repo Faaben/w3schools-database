1e3858ba0c7ff5ded19c684898786f47d5c2710e
Fabian Aeschimann <fabian.aeschimann@bluewin.ch>
2024-10-16 20:03:43 +0200

    Button farbe auf blau und gelb, schriftfarbe auf schwarz geändert

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -183,13 +183,13 @@ function CategoryList() {
                 ) : (
                   <div className="flex space-x-2">
                     <button 
-                      className="bg-blue-700 text-white px-4 py-2 rounded"
+                      className="bg-blue-500 text-black px-4 py-2 rounded"
                       onClick={() => setEditableCategoryId(category.CategoryID)}
                     >
                       Edit
                     </button>
                     <button 
-                      className="bg-red-500 text-white px-4 py-2 rounded"
+                      className="bg-yellow-500 text-black px-4 py-2 rounded"
                       onClick={() => handleDelete(category.CategoryID)}
                     >
                       Delete
